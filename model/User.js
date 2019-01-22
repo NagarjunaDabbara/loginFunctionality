41711609d9bad69f8e07a9ac77d09215a2de04d5
arjunchowdaryd <arjunchowdary.d@outlook.com>
2019-01-22 17:18:41 +0530

    jjj

--- model/User.js
+++ model/User.js
@@ -1,6 +1,6 @@
 const mongu = require('mongoose')
 
-mongu.connect('//mongodb://yourdbconnection-here', { useNewUrlParser: true })   //local/mlab mongoDB server
+mongu.connect("mongodb://arjun2:arjun2@ds259253.mlab.com:59253/mongouploads", { useNewUrlParser: true })   //local/mlab mongoDB server
 
 let userSchema = new mongu.Schema({
     name: {
